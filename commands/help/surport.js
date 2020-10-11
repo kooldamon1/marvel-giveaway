ccb110ac96f5241ddb97babeb04bfc469a8826ea
Glitch (eight-lovely-shoulder) <none>
2020-10-11 07:56:29 +0000

    🚟🐟 Checkpoint ./commands/help/surport.js:23585414/14 ./server.js:23585414/13745

--- commands/help/surport.js
+++ commands/help/surport.js
@@ -7,7 +7,7 @@ module.exports = {
     
     let embed = new discord.MessageEmbed()
     .setTitle(`SUPPORT SERVER <a:emoji_24:764200718344126546>`)
-    .setDescription(`[CLICK HERE](https://discord.gg/XXWtTxb)`)
+    .setDescription(`[CLICK HERE](https://discord.gg/3vxCNeR)`)
     .setColor("#ffffff")
     .setFooter(`ShaDoW Op`)
     .setTimestamp(message.timestamp = Date.now())
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const { prefix } = require("./config.json");
 const { config } = require("dotenv");
-const db =require("quick.db");
-const { CanvasSenpai } = require("canvas-senpai")
+const db = require("quick.db");
+const { CanvasSenpai } = require("canvas-senpai");
 const canva = new CanvasSenpai();
 const discord = require("discord.js");
 const client = new discord.Client({
@@ -18,21 +18,24 @@ client.aliases = new discord.Collection();
 
 client.on("ready", async () => {
   try {
-        console.log(client.user.tag + ' Has Logged In');
+    console.log(client.user.tag + " Has Logged In");
 
-        function pickStatus() {
-            let status = [`${client.users.cache.size} users in ${client.guilds.cache.size} servers`, 'shelp for commands', 'MADE BY :- SHADOW'];
-            let Status = Math.floor(Math.random() * status.length);
+    function pickStatus() {
+      let status = [
+        `${client.users.cache.size} users in ${client.guilds.cache.size} servers`,
+        "shelp ╏ ssupport",
+        "MADE BY :- SHADOW"
+      ];
+      let Status = Math.floor(Math.random() * status.length);
 
-            client.user.setActivity(status[Status], {
-                type: "WATCHING"
-            });
-        };
-        setInterval(pickStatus, 5000);
+      client.user.setActivity(status[Status], {
+        type: "WATCHING"
+      });
     }
-    catch (err) {
-        console.log(err);
-    }
+    setInterval(pickStatus, 5000);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 client.on("message", async message => {
@@ -41,7 +44,7 @@ client.on("message", async message => {
     return message.reply(`ANY HELP JUST USE shelp For Commands`);
   }
 
-  if (message.author.bot) return; 
+  if (message.author.bot) return;
   if (!message.guild) return;
   if (!message.content.startsWith(prefix)) return;
 
@@ -52,7 +55,7 @@ client.on("message", async message => {
     .slice(prefix.length)
     .trim()
     .split(/ +/g);
-  const cmd = args.shift().toLowerCase(); 
+  const cmd = args.shift().toLowerCase();
 
   if (cmd.length === 0) return;
 
@@ -63,55 +66,66 @@ client.on("message", async message => {
   if (command) command.run(client, message, args);
 });
 
-
-client.on("guildMemberAdd", async (member) => {
+client.on("guildMemberAdd", async member => {
   let chx = db.get(`welchannel_${member.guild.id}`);
-  
-  if(chx === null) {
+
+  if (chx === null) {
     return;
   }
-  
-  let default_url = `https://cdn.discordapp.com/attachments/696417925418057789/716197399336583178/giphy.gif`
-  
-  let default_msg = `━━━━━━━━━━━━━━━━━━━━━━━━
-  | WELCOME ${member} TO ${member.guild}
+
+  let default_url = `https://cdn.discordapp.com/attachments/696417925418057789/716197399336583178/giphy.gif`;
+
+  let default_msg = `
+━━━━━━━━━━━━━━━━━━━━━━━━
+
+ | WELCOME ${member} TO ${member.guild}
         
 ━━━━━━━━━━━━━━━━━━━━━━━━
- | BE SURE THAT YOU HAVE READ    
-           | 
+
+ | BE SURE THAT YOU HAVE READ |
+
 ━━━━━━━━━━━━━━━━━━━━━━━━
+
  | Usernam ${member.username}  
- | Your Rank Is ${member.member_count}  ━━━━━━━━━━━━━━━━━━━━━━━━
- | YOU CAN ENJOY IN  CHATTING 
+ | Your Rank Is ${member.member_count}  
+
 ━━━━━━━━━━━━━━━━━━━━━━━━
-        THANKS FOR JOINING US
-`
-  
-  let m1 = db.get(`msg_${member.guild.id}`)
-if(!m1) m1 = default_msg;
-const msg = m1.replace("{member}", member.user).replace("{member.guild}", member.guild).replace("(:HEART)",`<a:BH:731369456634429493>`)
 
-  
-  let url = db.get(`url_${member.guild.id}`)
-  if(url === null) url = default_url
-  
-   let data = await canva.welcome(member, { link: "https://wallpapercave.com/wp/wp5128415.jpg" })
- 
-    const attachment = new discord.MessageAttachment(
-      data,
-      "welcome-image.png"
-    );
-  
+ | YOU CAN ENJOY IN  CHATTING 
+
+━━━━━━━━━━━━━━━━━━━━━━━━
+
+        **__THANKS FOR JOINING US__**
+`;
+
+  let m1 = db.get(`msg_${member.guild.id}`);
+  if (!m1) m1 = default_msg;
+  const msg = m1
+    .replace("{member}", member.user)
+    .replace("{member.guild}", member.guild)
+    .replace("(:HEART)", `<a:BH:731369456634429493>`);
+
+  let url = db.get(`url_${member.guild.id}`);
+  if (url === null) url = default_url;
+
+  let data = await canva.welcome(member, {
+    link: "https://wallpapercave.com/wp/wp5128415.jpg"
+  });
+
+  const attachment = new discord.MessageAttachment(data, "welcome-image.png");
+
   let wembed = new discord.MessageEmbed()
-  .setAuthor(member.user.username, member.user.avatarURL({dynamic: true, size: 2048}))
-  .setThumbnail(member.user.displayAvatarURL({dynamic: true, size: 2048}))
-  .setColor("RANDOM")
-  .setImage()
-  .setDescription(msg);
-  
-  client.channels.cache.get(chx).send(wembed)
-  client.channels.cache.get(chx).send(attachment)
-})
+    .setAuthor(
+      member.user.username,
+      member.user.avatarURL({ dynamic: true, size: 2048 })
+    )
+    .setThumbnail(member.user.displayAvatarURL({ dynamic: true, size: 2048 }))
+    .setColor("RANDOM")
+    .setImage()
+    .setDescription(msg);
 
+  client.channels.cache.get(chx).send(wembed);
+  client.channels.cache.get(chx).send(attachment);
+});
 
 client.login(process.env.TOKEN);
