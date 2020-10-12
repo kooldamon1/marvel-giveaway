b537a0fe87a1870a834a21e9b3fea44bed108222
Glitch (eight-lovely-shoulder) <none>
2020-10-12 08:57:46 +0000

    🚄🎌 Checkpoint ./commands/help/helpnew.js:23585414/7332

--- commands/help/helpnew.js
+++ commands/help/helpnew.js
@@ -8,9 +8,34 @@ module.exports = {
   run: async (client, message, args) => {
     let embed = new discord.MessageEmbed()
       .setTitle(`BOT HELP MENU <a:India:764200316156510218> `)
+
+      .addField(
+        "MODERATION COMMANDS <a:op11:764200185253199892>",
+        `
+helpmod - shows moderation commands menu
+`
+      )
+
+      .addField(
+        "FUN COMMANDS <a:wink_pikachu:764199916456902666>",
+        `helpfun - shows funcommands menu`
+      )
+
+      .addField(
+        "INFORMATION COMMANDS <a:wink_pikachu:764199916456902666>",
+        `
+helpinfo - shows information commands menu
+`
+      )
+
+      .addField(
+        "MUSIC COMMANDS <a:musicau:764209002741235745>",
+        `       helpmusic - shows music comands menu
+`
+      )
     
-  .addField("MODERATION COMMANDS <a:op11:764200185253199892>",
-`\lpmod)
+    .addFieldd(""
+    )
 
       .addField(
         "BOT DEVELOPER <a:vshield:764199958257336321>",
