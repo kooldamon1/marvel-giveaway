0ecd126c91b6db1f0caaec4b5e71d941a2d1e860
Glitch (eight-lovely-shoulder) <none>
2020-10-12 16:05:34 +0000

    🚡🎣 Checkpoint ./commands/help/surport.js:23585414/40

--- commands/help/surport.js
+++ commands/help/surport.js
@@ -6,7 +6,7 @@ module.exports = {
   run: async (client, message, args) => {
     let embed = new discord.MessageEmbed()
       .setTitle(`SUPPORT SERVER <a:tick1:764199777281114133>`)
-      .setDescription(`[CLICK HERE](https://discord.gg/3vxCNeR)`)
+      .setDescription(`[CLICK HERE](https://discord.gg/FYQ2mrt)`)
       .setColor("#ffffff")
       .setFooter(`ShaDoW Op`)
       .setTimestamp((message.timestamp = Date.now()));
